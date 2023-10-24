897d609a9a3de02337c7d95c642026bf3329927f
mhimpel <gimpelllll@gmail.com>
2023-10-24 19:36:34 +0200

    Add logger

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
 import TelegramBot from 'node-telegram-bot-api';
-import { Config } from './config';
+import { logger } from 'core';
+import { Config } from 'config';
 
-// Create a new bot instance
 const bot = new TelegramBot(Config.BOT_TOKEN, { polling: true });
 
-// Event listener for when a user sends a message
 bot.on('message', message => {
+  logger.info('Message recived', message);
   const chatId = message.chat.id;
   const messageText = message.text;
 
-  // Reply to the user with their message
   bot.sendMessage(chatId, `You said: ${messageText}`);
 });
 
-// To handle errors
 bot.on('polling_error', error => {
-  console.error(error);
+  logger.error(error);
 });
 
-// Graceful shutdown
 process.once('SIGINT', () => {
   bot.stopPolling();
-  console.log('Bot has stopped.');
+  logger.info('Bot has stopped.');
 });
